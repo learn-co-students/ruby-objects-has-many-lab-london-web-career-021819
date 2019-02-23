03c10a54a450358dcd418316c3b33d51085c0ed5
github username <vyskoutaris@hotmail.com>
2019-02-23 09:14:43 +0000

    Done

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+
+class Author
+
+  attr_accessor :name, :post
+
+  @@posts = []
+
+  def initialize(name)
+    @name = name
+    @post = post
+  end
+
+  def posts
+    @@posts
+  end
+
+  def add_post(post)
+    @@posts << post
+    post.author = self
+  end
+
+def add_post_by_title(title)
+   post = Post.new(title)
+   @@posts  << post
+   post.author = self
+end
+
+def self.post_count
+  Song.all.length
+end
+
+end
