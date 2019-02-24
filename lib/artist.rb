86cf0c951be79b08417f239727fe12a7fdd1869c
github username <github email address>
2019-02-24 11:26:18 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,35 @@
 class Artist
 
-  attr_accessor :name, :songs
+  attr_accessor :name
 
-  @@all_songs = 0
+  @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 
   def add_song(song)
-      @songs << song
       song.artist = self #access the artist attribute on this song, and assign it to me using the = sign
-      @@all_songs +=1
   end
 
   def add_song_by_name(song)
     song = Song.new(song)
-    @songs << song
     song.artist = self
-    @@all_songs +=1
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self }
+
   end
 
   def self.song_count
-    @@all_songs
+    Song.all.count
   end
 
 end
